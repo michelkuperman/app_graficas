c27e3f997af7cc8cd22bc0f4c1b60e7a2b23f3e1
chepson <michelkuper@hotmail.com>
2012-07-01 23:01:21 -0500

    Initial commit app graficas

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppGraficas::Application.config.secret_token = '4ce4fa94d10a2d6de7890f76aa8d838ab51b005da333d151abb6dbf704e90bb896eda6d912e096e64414f1743b63762f0177e3652cf4d5f62f5e573b9f4557c5'
